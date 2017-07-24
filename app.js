d8c23bec421debf83ba01f6beb844db148ed3e6d
hhdem <liubingchina@gmail.com>
2017-07-24 14:02:23 +0800

    New code for Forum, Adjust some styles of page. Fix a bug that forum list at index page.

--- app.js
+++ app.js
@@ -183,7 +183,7 @@ if (config.debug) {
 
 if (!module.parent) {
     app.listen(config.port, function () {
-        logger.info('NodeClub listening on port', config.port);
+        logger.info('PinClub listening on port', config.port);
         logger.info('God bless love....');
         logger.info('You can debug your app with http://' + config.hostname + ':' + config.port);
         logger.info('');
